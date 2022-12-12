43078fd3d41f363005771ebc5ad785874dab2c63
ELBO0527 <63278770+ELBO0527@users.noreply.github.com>
2022-12-12 22:58:40 +0900

    set

---
+++ src/test/java/elbo/dotger/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package elbo.dotger.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
